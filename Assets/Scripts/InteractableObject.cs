b0c6b44a91666ddf80e729b21592653210bb967f
Christian Irvine <christian.irvine04@gmail.com>
2024-06-10 20:17:55 +1200

    Added InteractableObject script and displaying its fields on the UI

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string title;
+    public string description;
+}
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 //This script is to go onto the players camera
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private float interactionDistance;
+    [SerializeField] private TextMeshProUGUI interactionTextTitle;
+    [SerializeField] private TextMeshProUGUI interactionTextDescription;
+
+    private void Awake()
+    {
+        SetInteractionText("");
+    }
 
     void FixedUpdate()
     {
@@ -15,6 +23,32 @@ public class PlayerInteraction : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactionDistance))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+
+            InteractableObject interactableObject = hit.transform.GetComponent<InteractableObject>();
+
+            if (interactableObject != null)
+            {
+                SetInteractionText(interactableObject.title, interactableObject.description);
+            }
+            else
+            {
+                SetInteractionText(hit.transform.name);
+            }
         }
+        else
+        {
+            SetInteractionText("");
+        }
+    }
+
+    private void SetInteractionText(string title, string description)
+    {
+        interactionTextTitle.text = title;
+        interactionTextDescription.text = description;
+    }
+
+    private void SetInteractionText(string title)
+    {
+        SetInteractionText(title, "");
     }
 }
