de5d03cb1210d90bab10f50cabf65292b6af7d33
Christian Irvine <christian.irvine04@gmail.com>
2024-06-10 19:14:05 +1200

    Added horizontal movement of player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed;
+    private InputAction moveAction;
+
+    private void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        transform.position += new Vector3(moveValue.x * moveSpeed * Time.deltaTime, 0, moveValue.y * moveSpeed * Time.deltaTime);
+
     }
 }
