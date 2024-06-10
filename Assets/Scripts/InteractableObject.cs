8df906eb6d17c52c5ec0a88e453d50fe5ab99317
Christian Irvine <christian.irvine04@gmail.com>
2024-06-10 20:38:52 +1200

    Added functionality of IsInteractable button

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -6,4 +6,13 @@ public class InteractableObject : MonoBehaviour
 {
     public string title;
     public string description;
+    public bool isInteractable;
+    public string interactableText = "Press (X) To Interact"; // This is the default but can be changed in Inspector, using X here for an example we can change this later when we pick a default
+
+    // This method is made to be overridden if interactable is toggled true
+    // This is for anything the player will push a button on to do something with
+    public virtual void Interact()
+    {
+
+    }
 }
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 
 //This script is to go onto the players camera
@@ -10,6 +11,7 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] private float interactionDistance;
     [SerializeField] private TextMeshProUGUI interactionTextTitle;
     [SerializeField] private TextMeshProUGUI interactionTextDescription;
+    [SerializeField] private TextMeshProUGUI interactionTextButton;
 
     private void Awake()
     {
@@ -28,7 +30,7 @@ public class PlayerInteraction : MonoBehaviour
 
             if (interactableObject != null)
             {
-                SetInteractionText(interactableObject.title, interactableObject.description);
+                SetInteractionDisplay(interactableObject);
             }
             else
             {
@@ -41,10 +43,34 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-    private void SetInteractionText(string title, string description)
+    private void SetInteractionDisplay(InteractableObject interactableObject)
+    {
+        if (interactableObject.isInteractable)
+        {
+            SetInteractionText(interactableObject.title, interactableObject.description, interactableObject.interactableText);
+            
+            //Do a check here when the interact key is pressed down once the input system is implimented
+            //if (Check For Input)
+            //{
+            //    interactableObject.Interact();
+            //}
+        }
+        else
+        {
+            SetInteractionText(interactableObject.title, interactableObject.description);
+        }
+    }
+
+    private void SetInteractionText(string title, string description, string button)
     {
         interactionTextTitle.text = title;
         interactionTextDescription.text = description;
+        interactionTextButton.text = button;
+    }
+
+    private void SetInteractionText(string title, string description)
+    {
+        SetInteractionText(title, description, "");
     }
 
     private void SetInteractionText(string title)
