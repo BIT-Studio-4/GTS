ae0bfe3a633a2c74be5e3c9a6557dde40dc71869
Christian Irvine <christian.irvine04@gmail.com>
2024-06-19 13:17:42 +1200

    Fixed InteractableObject to have [SerializeField] fields

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -8,18 +8,18 @@ using UnityEngine;
 /// </summary>
 public class InteractableObject : MonoBehaviour
 {
-    private string title;
+    [SerializeField] private string title;
     public string Title { get { return title; } set { title = value; } }
 
-    private string description;
+    [SerializeField] private string description;
     public string Description { get { return description; } set { description = value; } }
 
     [Tooltip("Gives the ability to run the interact method via the interact button")]
-    private bool isInteractable;
+    [SerializeField] private bool isInteractable;
     public bool IsInteractable { get { return isInteractable; } set { isInteractable = value; } }
 
     // This is the default but can be changed in Inspector, using X here for an example we can change this later when we pick a default
-    private string interactableText = "Press (X) To Interact"; 
+    [SerializeField] private string interactableText = "Press (X) To Interact"; 
     public string InteractableText { get { return interactableText; } set { interactableText = value; } }
 
     // This method is made to be overridden if interactable is toggled true
