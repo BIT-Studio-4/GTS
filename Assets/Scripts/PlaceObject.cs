d9d2ae01570fce1c1aa0a3885dd28057e7352bde
otherotherjosh <itsotherjosh@gmail.com>
2024-07-03 22:18:18 +1200

    feat(cs): place item north of player
    bottles!!

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -5,13 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlaceObject : MonoBehaviour
 {
+    [SerializeField] List<GameObject> prefabs = new List<GameObject>();
+
     void Awake()
     {
-        InputSystem.actions.FindAction("Place").performed += ctx => InstantiateObject();
+        InputSystem.actions.FindAction("Place").performed += ctx => InstantiateObject(ctx);
     }
 
-    void InstantiateObject(/*InputAction.CallbackContext ctx*/)
+    void InstantiateObject(InputAction.CallbackContext ctx)
     {
-        GameObject placedObject = new GameObject();
+        GameObject placedObject = Instantiate(prefabs[0], transform.position + Vector3.forward, Quaternion.identity);
     }
 }
