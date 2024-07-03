8c28202c6beb1b7b9ba61ade1161780afac4782f
otherotherjosh <itsotherjosh@gmail.com>
2024-07-03 23:08:23 +1200

    doc(cs): place objects as children
    Placed objects are now children of empty game object "Placed Objects" (generated in script), so as to not clog up the hierarchy in editor

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class PlaceObject : MonoBehaviour
 {
     [SerializeField] List<GameObject> prefabs = new List<GameObject>();
+    private GameObject placedObjects;
 
     void Awake()
     {
+        placedObjects = new GameObject("Placed Objects"); // empty parent to keep all instantiated objects hidden in hierarchy
         InputSystem.actions.FindAction("Place").performed += ctx => InstantiateObject(ctx);
     }
 
@@ -16,11 +18,11 @@ public class PlaceObject : MonoBehaviour
     {
         RaycastHit hit;
 
-        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10))
+        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10)) // hard coded interaction distance (10)
         {
             return;
         }
 
-        GameObject placedObject = Instantiate(prefabs[0], hit.point, Quaternion.identity);
+        GameObject placedObject = Instantiate(prefabs[0], hit.point, Quaternion.identity, placedObjects.transform);
     }
 }
