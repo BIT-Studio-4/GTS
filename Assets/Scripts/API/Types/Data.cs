7015980c1b8957f3535446d7e09680ceed56c43f
wilsjw <wilsjw2@student.op.ac.nz>
2024-07-03 23:59:52 +1200

    docs: Added summary to describe Data class

--- Assets/Scripts/API/Types/Data.cs
+++ Assets/Scripts/API/Types/Data.cs
@@ -1,5 +1,11 @@
 using System;
 
+/// <summary>
+/// Required when retrieving data from the game's API to destructure the result correctly.
+/// </summary>
+/// <typeparam name="T">
+/// Use the desired 'table' type as this will store the require data in <c>data</c>.
+/// </typeparam>
 
 [Serializable]
 public class Data<T>
