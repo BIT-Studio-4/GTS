bd6fa98b068039534b6a3c35a23dec8709c2c872
wilsjw <wilsjw2@student.op.ac.nz>
2024-07-03 21:16:12 +1200

    docs: Added descriptions to unfamiliar lines or processes

--- Assets/Scripts/API/HTTPRequests.cs
+++ Assets/Scripts/API/HTTPRequests.cs
@@ -9,16 +9,24 @@ public static class HTTPRequests<T>
 {
     public static async Task<T> Get(string url)
     {
+        // Create a new GET request.
+        // Similar to the 'options' parameter of a javascript request.
         UnityWebRequest http = UnityWebRequest.Get(url);
         http.method = "GET";
         http.SetRequestHeader("Content-Type", "application/json");
 
+        // Make the request to the API.
+        // Similar to running 'fetch(url, options)'.
         UnityWebRequestAsyncOperation req = http.SendWebRequest();
 
-        while (!req.isDone) await Task.Yield();
+        // Wait until request is finished.
+        // 'Task.Yield()' waits until the condition is true to continue without slowing down other processes.
+        while (!req.isDone) await Task.Yield(); 
 
         string response = http.downloadHandler.text;
 
+        // Check if the request was successful.
+        // If not, early return with an error log.
         if (http.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError(http.error);
@@ -26,6 +34,7 @@ public static class HTTPRequests<T>
             return default;
         }
 
+        // Attempt to parse the JSON result to an Object that represents the data.
         try
         {
             T result = JsonUtility.FromJson<T>(response);
@@ -38,7 +47,7 @@ public static class HTTPRequests<T>
         {
             Debug.LogError(ex.Message);
 
-            return default;
+            return default; // Essentially returning null but ensuring that non-nullable types still work.
         }
     }
 }
