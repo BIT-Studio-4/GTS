1a03363b96eeb36f65f2b820a2940c3a96285486
Christian Irvine <christian.irvine04@gmail.com>
2024-07-03 19:47:25 +1200

    Made PlaceableObject script and made StoreInventory a singleton

--- Assets/Scripts/StoreInventory.cs
+++ Assets/Scripts/StoreInventory.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class StoreInventory : MonoBehaviour
 {
+    public static StoreInventory Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
