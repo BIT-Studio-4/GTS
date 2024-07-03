ed003b308100863e5a6b22d24f95459c90ca6093
wilsjw <wilsjw2@student.op.ac.nz>
2024-07-03 21:46:47 +1200

    docs: Added descriptions and summary to User

--- Assets/Scripts/API/Types/User.cs
+++ Assets/Scripts/API/Types/User.cs
@@ -1,7 +1,10 @@
 using System;
-using UnityEngine;
 
-[Serializable]
+/// <summary>
+/// Allows user info from a JSON response to be stored in a type safe and specific instance.
+/// </summary>
+
+[Serializable] // Means that data can be parsed and stored within an instance of this class of creation.
 public class User
 {
     public string id;
