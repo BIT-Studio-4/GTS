4051b3f430693e86308f26260df20a2e8352df94
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2024-07-04 11:42:51 +1200

    made basic GridItem and grid for inventory

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -10,11 +10,13 @@ public class InventoryManager : MonoBehaviour
 
     [SerializeField] private List<PlaceableObject> placeableObjects = new List<PlaceableObject>();
     [SerializeField] private GameObject inventoryGUI;
+    [SerializeField] private GameObject inventoryGrid;
+    [SerializeField] private GameObject inventoryItemPrefab;
 
     private PlaceableObject heldObject;
     public PlaceableObject HeldObject { get { return heldObject; } set { heldObject = value; } }
 
-    private int tabIndex = 0;
+    private int tabIndex;
 
     private void Awake()
     {
@@ -32,17 +34,26 @@ public class InventoryManager : MonoBehaviour
         inventoryGUI.SetActive(false);
         HeldObject = null;
         InputSystem.actions.FindAction("ToggleInventory").performed += ctx => ToggleInventoryGUI();
+        SwitchTab(0);
     }
-
+    
     private void ToggleInventoryGUI()
     {
         inventoryGUI.SetActive(!inventoryGUI.activeSelf);
 
         Cursor.lockState = inventoryGUI.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
+        SwitchTab(tabIndex);
     }
 
     public void SwitchTab(int index)
     {
         tabIndex = index;
+        Debug.Log($"New Tab Index is {index}");
+        SetInventoryContent();
+    }
+
+    private void SetInventoryContent()
+    {
+        
     }
 }
