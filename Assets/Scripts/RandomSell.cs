60734811aff24446fff1880444cba89d54fa22cd
lolyay270 <boyeje2@student.op.ac.nz>
2024-07-04 14:59:05 +1200

    fix (sell): changed variables to public and fixed spelling

--- Assets/Scripts/RandomSell.cs
+++ Assets/Scripts/RandomSell.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 
 public class RandomSell : MonoBehaviour
 {
-    [SerializeField] private float avgSellTimeSecs, varienceSellTimeSecs;
-    [SerializeField] private int moneyOnSell;
+    //these will be set by "PlaceObject" script when object spawns
+    public float avgSellTimeSecs, varianceSellTimeSecs;
+    
+    //this will be set by the stock item or stock list eventually
+    public int moneyOnSell;
 
     private IEnumerator Start()
     {
         //wait random time
-        float varience = Random.Range(-varienceSellTimeSecs, varienceSellTimeSecs);
-        float waitTimeTotal = avgSellTimeSecs + varience;
+        float variance = Random.Range(-varianceSellTimeSecs, varianceSellTimeSecs);
+        float waitTimeTotal = avgSellTimeSecs + variance;
         yield return new WaitForSeconds(waitTimeTotal);
 
         //destroy object and give player money
