8b4f4ef323b8459b15d6cf537de62d51822d3882
Christian Irvine <christian.irvine04@gmail.com>
2024-07-30 11:45:06 +1200

    Made UIManager a singleton

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    
+    public static UIManager Instance;
+
+    [SerializeField] private GameObject storeUI;
+    [SerializeField] private GameObject inventoryUI;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
 }
