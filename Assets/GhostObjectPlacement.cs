2fef7ddf06e290c5bd502994954e101f09cbb803
gracedong77 <132322996+otherotherjosh@users.noreply.github.com>
2024-08-13 14:57:08 +1200

    refactor(cs): makes raycast hit a public accessor

---
+++ Assets/GhostObjectPlacement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlaceObject))]
+[RequireComponent(typeof(PlayerInteraction))]
+public class GhostObjectPlacement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
