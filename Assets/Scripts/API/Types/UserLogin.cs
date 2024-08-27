61bf266926fe1ae5f2f07b037446c4caaaab60fc
wilsjw <wilsjw2@student.op.ac.nz>
2024-08-27 14:24:17 +1200

    feat: Created new UserLogin class to send login info to API

---
+++ Assets/Scripts/API/Types/UserLogin.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Allows user login info to be sent correctly to API.
+/// </summary>
+
+[Serializable] // Means that data can be parsed and stored within an instance of this class of creation.
+public class UserLogin
+{
+    public string name;
+    public string password;
+}
