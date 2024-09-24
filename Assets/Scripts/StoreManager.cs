2f9c96248c8fb5df9884dc50bd621a4d14633906
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2024-09-24 14:06:04 +1200

    Added some summary comments to StoreManager

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -31,7 +31,7 @@ public class StoreManager : MonoBehaviour
     private int tabIndex = 0;
     private int totalCost = 0;
     private List<GameObject> gridObjectDisplayList = new List<GameObject>();
-    // A list of all the counts of items in the cart
+    // A list of how many of each item there is in the players cart
     private List<int> itemCountsInCart = new List<int>();
     private int countMultiplier;
 
@@ -68,7 +68,7 @@ public class StoreManager : MonoBehaviour
         // When the store GUI is opened
         if (storeGUI.activeSelf)
         {
-            FillPurchaseItemList();
+            FillShoppingCartWithItems();
             SwitchTab(tabIndex);
             totalCost = CalculateTotalCost();
             totalCostText.text = $"Total: ${totalCost}";
@@ -111,17 +111,19 @@ public class StoreManager : MonoBehaviour
         gridObjectDisplayList.ForEach(gridItem => Destroy(gridItem));
         gridObjectDisplayList.Clear();
 
-        int UIIndex = 0;
+        int indexInTab = 0;
         int storeIndex = 0;
+
+        
 
         // Iterates over all items to see if it should display in current tab
         allStoreItems.ForEach(placeableObject =>
         {
             if (((int)placeableObject.type) == tabIndex)
             {
-                CreateGridItem(UIIndex, storeIndex, placeableObject);
+                CreateGridItem(indexInTab, storeIndex, placeableObject);
 
-                UIIndex++;
+                indexInTab++;
             }
 
             storeIndex++;
@@ -129,22 +131,27 @@ public class StoreManager : MonoBehaviour
     }
 
     // Instantiates a new grid GameObject in the Store menu
-    private void CreateGridItem(int UIIndex, int storeIndex, StoreItemSO storeItem)
+    private void CreateGridItem(int indexInTab, int storeIndex, StoreItemSO storeItem)
     {
         // Creates the new GameObject and puts it in a list
         GameObject gridItem = Instantiate(storeItemPrefab, storeGrid.transform);
         gridObjectDisplayList.Add(gridItem);
         StoreItemSlot gridSlot = gridItem.GetComponent<StoreItemSlot>();
 
-        gridSlot.AddButton.onClick.AddListener(() => ChangeStockCount(UIIndex, storeIndex, 1));
-        gridSlot.SubtractButton.onClick.AddListener(() => ChangeStockCount(UIIndex, storeIndex, -1));
+        gridSlot.AddButton.onClick.AddListener(() => ChangeStockCount(indexInTab, storeIndex, 1));
+        gridSlot.SubtractButton.onClick.AddListener(() => ChangeStockCount(indexInTab, storeIndex, -1));
         gridSlot.NameText.text = storeItem.name;
         gridSlot.PriceText.text = $"${storeItem.cost}";
         gridSlot.CountText.text = $"{itemCountsInCart[storeIndex]}";
     }
 
-    // Runs when the amount of stock the player wants is added to or subtracted from
-    public void ChangeStockCount(int UIIndex, int storeIndex, int change)
+    /// <summary>
+    /// Changes the stock count in your cart based on the currently selected multiplier
+    /// </summary>
+    /// <param name="indexInTab">The index of the changed item in its tab</param>
+    /// <param name="storeIndex">The index of the changed item in the store</param>
+    /// <param name="change">The amount the count is changed by</param>
+    public void ChangeStockCount(int indexInTab, int storeIndex, int change)
     {
         if (countMultiplier > 1 && change > 0) //stop higher than 1 multipliers from adding more than can afford
         {
@@ -155,12 +162,11 @@ public class StoreManager : MonoBehaviour
         }
         else if (change < 0) change *= countMultiplier; //reduce stock by multiplier
 
-
         int itemCount = itemCountsInCart[storeIndex];
         itemCount = Mathf.Max(itemCount + change, 0); //cant be less than 0
         itemCountsInCart[storeIndex] = itemCount;
 
-        StoreItemSlot slot = gridObjectDisplayList[UIIndex].GetComponent<StoreItemSlot>();
+        StoreItemSlot slot = gridObjectDisplayList[indexInTab].GetComponent<StoreItemSlot>();
         slot.CountText.text = itemCount.ToString();
 
         totalCost = CalculateTotalCost();
@@ -169,25 +175,34 @@ public class StoreManager : MonoBehaviour
         UpdateMoneyColors();
     }
 
-    // Changes the tab and resets the contents of the store
+    /// <summary>
+    /// Changes the tab and resets the contents of the store
+    /// </summary>
+    /// <param name="index"></param>
     public void SwitchTab(int index)
     {
         tabIndex = index;
         SetStoreDisplayContent();
     }
 
-    // Fills the list of all empty items possible to purchase
-    private void FillPurchaseItemList()
+    /// <summary>
+    /// Fills the shopping cart with a list of 0 count items
+    /// </summary>
+    private void FillShoppingCartWithItems()
     {
         itemCountsInCart.Clear();
 
+        // Fills the cart with empty numbers
         allStoreItems.ForEach(item =>
         {
             itemCountsInCart.Add(0);            
         });
     }
 
-    // Calculates the total cost of all selected items to purchase
+    /// <summary>
+    /// Calculates the total cost of all selected items to purchase
+    /// </summary>
+    /// <returns>The total cost of current items in the cart</returns>
     private int CalculateTotalCost()
     {
         int totalCost = 0;
