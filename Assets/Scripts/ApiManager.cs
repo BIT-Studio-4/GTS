84b2225f195eeb14239a9972cce3271c3d50af60
wilsjw <wilsjw2@student.op.ac.nz>
2024-10-15 19:12:51 +1300

    fix: Removed unnecessary Test object in ApiManager

--- Assets/Scripts/ApiManager.cs
+++ Assets/Scripts/ApiManager.cs
@@ -11,17 +11,6 @@ public class ApiManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-
-        Test test = new()
-        {
-            name = "hi",
-            age = 5,
-            money = 10.5,
-            Children = new[] { "hello", "hey", "hiya" },
-            User = new User { name = "test-user", id = "0", Money = 200, token = "example-token"},
-        };
-
-        Debug.Log(HTTPRequests.GetJson(test));
     }
 
     public async void UpdateUser(string url, User user)
