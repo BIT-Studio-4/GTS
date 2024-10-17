d362c4ed7a6767d9327aad7e2fe3afa764c1ec90
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2024-10-18 11:55:29 +1300

    add reference to scripts

--- Assets/Tests/TestScript.cs
+++ Assets/Tests/TestScript.cs
@@ -6,20 +6,15 @@ using UnityEngine.TestTools;
 
 public class TestScript
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TestScriptSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
-
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator TestScriptWithEnumeratorPasses()
+    public IEnumerator SellingPrices()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
         yield return null;
+
+        
     }
 }
