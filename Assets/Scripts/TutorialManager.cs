3765b119b59c2e994def36c3790e58f947a486b3
otherotherjosh <132322996+otherotherjosh@users.noreply.github.com>
2024-11-05 14:38:52 +1300

    fix: make readable

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -14,6 +15,8 @@ public class TutorialManager : MonoBehaviour
     private Dictionary<string, bool> tutorial;
     public Dictionary<string, bool> Tutorial { get => tutorial; }
 
+    private Coroutine fade;
+    private Coroutine endTutorial;
 
     void Awake()
     {
@@ -49,43 +52,41 @@ public class TutorialManager : MonoBehaviour
     {
         if (!tutorial["openedInventory"])
         {
-            TutorialText.text = "Welcome to your very own supermarket!\n\nTo get started, press [Tab] to open your inventory\n\n(hold [N] to close tutorial)";
+            TutorialText.text = "Welcome to your very own supermarket!\nTo get started, press [Tab] to open your inventory\n(hold [N] to close tutorial)";
             return;
         }
         if (!tutorial["selectedShelf"])
         {
-            TutorialText.text = "We have provided you with your first shelf!\n\nFind it by going to the Structures section";
+            TutorialText.text = "We have provided you with your first shelf!\nFind it by going to the Structures section";
             return;
         }
         if (!tutorial["placedShelf"])
         {
-            TutorialText.text = "Place your shelf anywhere you want!\n\nYou can rotate using the scroll wheel, or go off-grid with [Ctrl]";
+            TutorialText.text = "Place your shelf anywhere you want!\nYou can rotate using the scroll wheel, or go off-grid with [Ctrl]";
             return;
         }
         if (!tutorial["openedShop"])
         {
-            TutorialText.text = "Good job! Now we want to buy stock so we can sell something\n\nPress [Q] to open the shop screen";
+            TutorialText.text = "Good job! Now we want to buy stock so we can sell something\nPress [Q] to open the shop screen";
             return;
         }
         if (!tutorial["boughtStock"])
         {
-            TutorialText.text = "From the Stock section, buy anything you want!\n\nUse the [x10] multiplier to make bulk purchases faster";
+            TutorialText.text = "From the Stock section, buy anything you want!\nUse the [x10] multiplier to make bulk purchases faster";
             return;
         }
         if (!tutorial["placedStock"])
         {
-            TutorialText.text = "Nice!\n\nGrab stock from your inventory [Tab] and place it on your shelf";
+            TutorialText.text = "Nice!\nGrab stock from your inventory [Tab] and place it on your shelf";
             return;
         }
         if (!tutorial["soldStock"])
         {
-            TutorialText.text = "Awesome!\n\nNow we wait for customers to come in and buy what's on the shelf";
+            TutorialText.text = "Awesome!\nNow we wait for customers to come in and buy what's on the shelf";
             return;
         }
 
-        TutorialText.text = "That is the end of the tutorial!\n\nGo and make some profit!!";
-
-        Invoke("HideTutorial", 1);
+        endTutorial = StartCoroutine(EndTutorial());        
     }
 
     // Method to hide the tutorial
@@ -109,5 +110,10 @@ public class TutorialManager : MonoBehaviour
         {
             CompleteTutorialTask(task);
         }
+    }
+
+    IEnumerator EndTutorial()
+    {
+        yield return null;
     }
 }
